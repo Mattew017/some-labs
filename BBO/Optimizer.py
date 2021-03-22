bcd3a62ae484e117ddcb53dd4e0d00aa976f48af
Mattew017 <zaelapporagrance@gmail.com>
2021-03-23 00:28:46 +0500

    change Optimezer.py

--- BBO/Optimizer.py
+++ BBO/Optimizer.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import time
 import myBBO
 from benchmark_functions import *
 
@@ -6,11 +8,45 @@ from benchmark_functions import *
 def rounder(lst):
     return [round(x, 5) for x in lst]
 
-pop_size = 30
-mutation_probability = 0.01
-search_params = {F1:[-100, 100, 30], F4 : [-100, 100, 30]}
 
+
+search_params = {
+    F1:[-100, 100, 30], F2 : [-100, 100, 30],
+    F3: [-10, 10, 30], F4: [-600, 600, 30], F5:[-100, 100, 30]
+    }
+
+result = {
+    "function": None,
+    "count of iters": None,
+    "population size": None,
+    "mutation probability": None,
+    "F(X)": None,
+    "X": None,
+    "time": None
+}
+
+data_frame = pd.DataFrame(columns=result.keys())
+
+start = time.time()
 for function in search_params.keys():
-    result = myBBO.BBO(function, 1000,pop_size,mutation_probability, search_params[function])
-    fitness = result[0]
-    params  = rounder(result[1])
+    for count_of_iter in range(100, 1000, 500):
+        for pop_size in range(5, 40, 10):
+            for m_p in np.arange(0, 0.5, 0.05):
+                start_time = time.time()
+                answer = myBBO.BBO(function, count_of_iter,pop_size,m_p, search_params[function])
+                total_time = round(time.time() - start_time, 5)
+                fitness = answer[0]
+                params  = rounder(answer[1])
+
+                result["function"] = function.__name__
+                result["count of iters"] = count_of_iter
+                result["population size"] = pop_size
+                result["mutation probability"] = m_p
+                result["F(X)"] = fitness
+                result["X"] = params
+                result["time"] = total_time
+
+                data_frame = data_frame.append(result, ignore_index = True)
+data_frame.to_csv('csv.csv')
+data_frame.to_excel('benchmark.xlsx')
+print(f'Total time of benchmarking: {time.time() - start}')
